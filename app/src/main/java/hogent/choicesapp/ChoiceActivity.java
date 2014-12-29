c2afc945730b77b440041028235ccd8ddffb7975
Christof Van Cauteren <christofvc@gmail.com>
2014-12-29 23:31:28 +0100

    Finished task 1 and 2

--- app/src/main/java/hogent/choicesapp/ChoiceActivity.java
+++ app/src/main/java/hogent/choicesapp/ChoiceActivity.java
@@ -1,19 +1,71 @@
 package hogent.choicesapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+public class ChoiceActivity extends Activity {
 
-public class ChoiceActivity extends ActionBarActivity {
+    private static final String[] ACTIVITY_CHOICES = new String[]{"Open web site","Open contacts","Open dialer","Search google","Start voice command"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+
+        ListView choices = (ListView) findViewById(R.id.list);
+        int layoutID = android.R.layout.simple_list_item_1;
+        ArrayAdapter<String> myAdapterInstance = new ArrayAdapter<String>(this,layoutID,ACTIVITY_CHOICES);
+        choices.setAdapter(myAdapterInstance);
+
+        setOnItemClickListener(choices);
     }
 
+    private void setOnItemClickListener(ListView listView) {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            final Intent intent = new Intent();
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        intent.setAction(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse("http://www.google.com"));
+                        break;
+                    case 1:
+                        intent.setAction(Intent.ACTION_PICK);
+                        intent.setData(ContactsContract.Contacts.CONTENT_URI);
+                        break;
+                    case 2:
+                        intent.setAction(Intent.ACTION_DIAL);
+                        intent.setData(Uri.parse("tel:0123456789"));
+                        break;
+                    case 3:
+                        intent.setAction(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse("http://www.google.com/#q=olaf%20frozen"));
+                        break;
+                    case 4:
+                        Log.e("lala", "lala");
+                        intent.setAction(Intent.ACTION_VOICE_COMMAND);
+                        break;
+                    default:
+                        throw new IllegalArgumentException();
+                }
+
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
